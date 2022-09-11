947f88c817296c0ed703fff579ae58a8baab6136
omid-aram <omid.aram@hotmail.com>
2022-09-11 09:15:18 +0430

    ..

--- src/app/partials/editors/InputDate.js
+++ src/app/partials/editors/InputDate.js
@@ -8,7 +8,7 @@ import { toPersianDateTime } from "../../utils/helper";
 
 
 
-const InputDate = (props) => { //JUST FOR TEST
+const InputDate = (props) => { //JUST FOR TEST2
     const { name, label, time,...rest } = props
    // const { control, errors } = useFormContext();
     const { control, errors, values } = useFormContext();
